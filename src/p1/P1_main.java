98fb4900e372c6006ad02cbd7911341b52738d0d
Julian Hodapp <julian@MacBook-Pro-von-Julian.local>
2025-10-27 13:28:52 +0100

    Praesensaufgaben Blatt 2 p1,p2,p3: Lösungen ohne erklärung

---
+++ src/p1/P1_main.java
@@ -0,0 +1,16 @@
+package p1;
+
+import java.util.Scanner;
+
+public class P1_main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
+	}
+	
+	//Lösung b: Das Programm scannt die eingaben im terminal und wenn man etwas eingibt gibt das Programm den eingegebenen String wieder und beendet sich automatisch
+	//
+}
